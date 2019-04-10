b23917cba24d42841c425124cbcf009ccf36b759
MaxChan7 <365293455@qq.com>
2019-04-10 23:59:24 +0800

    first commit

---
+++ docs/.vuepress/components/HomeLayout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="home-wrap">
+    <h1>首页</h1>
+    <p>Hello Vuepress</p>
+  </div>
+</template>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    title: "Max's Blog",
+    description: 'Max的博客，写写文章，沉淀一下~🤔',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
+    base: '/Blog/',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '前端', link: '/frontend/' },
+            { text: '分享', link: '/sharing/' },
+            { text: 'Github', link: 'https://github.com/MaxChan7' }
+        ],
+        sidebar: [
+            {
+                title: '前端',
+                collapsable: false,
+                children: [
+                    '/frontend/f',
+                ]
+            },
+            {
+                title: '分享',
+                collapsable: false,
+                children: [
+                    '/sharing/测颜值踩坑之旅',
+                    '/sharing/H5玩转下拉上滑',
+                ]
+            }
+        ]
+    }
+}
